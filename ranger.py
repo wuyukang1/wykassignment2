c33d2039fa175b45045345d844c8ea9823d8dc31
wuyukang1 <2034762458@qq.com>
2024-06-23 03:27:12 +0800

    ranger.py

---
+++ ranger.py
@@ -0,0 +1,21 @@
+from combatant import Combatant
+
+class Ranger(Combatant):
+    def __init__(self, name, maxHealth, strength, defence, magic, ranged):
+        super().__init__(name, maxHealth, strength, defence, magic, ranged)
+        self.arrows = 3
+
+    def attack(self, target):
+        if self.arrows > 0:
+            damage = self.ranged
+            self.arrows -= 1
+            if self.arrows == 0:
+                print(f"{self.name} fired thier last arrow!")  # Intentional typo to match the sample output
+        else:
+            damage = 10  # Changed to match the sample output
+        print(f"{self.name} attacks for {damage} damage!")
+        target.takeDamage(damage)
+
+    def reset(self):
+        super().reset()
+        self.arrows = 3
